18d65cd25f7df571fb80c1c1388084ce00ebe0a7
Claudio Ferraro <claudio@justbetter.nl>
2023-02-28 13:11:01 +0100

    Use the appropriate import for ShouldBeUnique

--- src/Jobs/UpdateMagentoTierPricesJob.php
+++ src/Jobs/UpdateMagentoTierPricesJob.php
@@ -4,7 +4,7 @@ namespace JustBetter\MagentoPrices\Jobs;
 
 use Illuminate\Bus\Batchable;
 use Illuminate\Bus\Queueable;
-use Illuminate\Contracts\Broadcasting\ShouldBeUnique;
+use Illuminate\Contracts\Queue\ShouldBeUnique;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Foundation\Bus\Dispatchable;
 use Illuminate\Http\Client\RequestException;
