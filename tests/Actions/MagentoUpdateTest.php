dd35e28baee92aec1f889283471b97d3c3178f2f
Vincent Boon <vincent@justbetter.nl>
2023-06-07 15:02:22 +0200

    Add async update option

--- tests/Actions/MagentoUpdateTest.php
+++ tests/Actions/MagentoUpdateTest.php
@@ -39,6 +39,33 @@ class MagentoUpdateTest extends TestCase
         });
     }
 
+    public function test_it_calls_magento_base_price_post_async(): void
+    {
+        config()->set('magento-prices.async', true);
+
+        Http::fake([
+            'rest/all/async/V1/products/base-prices*' => Http::response([]),
+        ]);
+
+        $price = new PriceData('::sku::', collect([new BasePriceData(Money::of(10, 'EUR'))]));
+
+        /** @var UpdateMagentoBasePrice $action */
+        $action = app(UpdateMagentoBasePrice::class);
+        $action->update($price);
+
+        Http::assertSent(function (Request $request) {
+            return $request->data() == [
+                'prices' => [
+                    [
+                        'sku' => '::sku::',
+                        'price' => 10.0,
+                        'store_id' => 0,
+                    ],
+                ],
+            ];
+        });
+    }
+
     public function test_it_calls_magento_tier_price_post(): void
     {
         Http::fake([
@@ -81,4 +108,52 @@ class MagentoUpdateTest extends TestCase
             },
         ]);
     }
+
+    public function test_it_calls_magento_tier_price_post_async(): void
+    {
+        config()->set('magento-prices.async', true);
+
+        Http::fake([
+            'rest/all/async/V1/products/tier-prices*' => Http::response([]),
+            'rest/all/V1/customerGroups/search*' => Http::response([
+                'items' => [
+                    [
+                        'code' => 'GROUP',
+                    ],
+                ],
+            ]),
+        ]);
+
+        $price = new PriceData('::sku::', collect(), collect([
+            new TierPriceData('GROUP', Money::of(10, 'EUR')),
+            new TierPriceData('GROUP2', Money::of(10, 'EUR')),
+        ]));
+
+        /** @var UpdateMagentoTierPrices $action */
+        $action = app(UpdateMagentoTierPrices::class);
+        $action->update($price);
+
+        Http::assertSentInOrder([
+            function (Request $request) {
+                return $request->url() === 'rest/all/V1/customerGroups/search?searchCriteria%5BpageSize%5D=100&searchCriteria%5BcurrentPage%5D=1';
+            },
+            function (Request $request) {
+                return $request->data() == [
+                    'prices' => [
+                        [
+                            'sku' => '::sku::',
+                            'price' => 10.0,
+                            'website_id' => 0,
+                            'quantity' => 1,
+                            'customer_group' => 'GROUP',
+                            'price_type' => 'fixed',
+                        ],
+                    ],
+                ];
+            },
+        ]);
+    }
+
+
+
 }
