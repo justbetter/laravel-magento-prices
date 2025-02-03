bf542ae08d5e1f0fea2c8e8890a0f3044c64e1be
Vincent Boon <vincent@justbetter.nl>
2025-02-03 09:45:37 +0100

    Optimize special price updates (#29)
    * Optimize special price updates
    
    * Adjust price data validaiton rule

--- src/Actions/Utility/DeleteCurrentSpecialPrices.php
+++ src/Actions/Utility/DeleteCurrentSpecialPrices.php
@@ -2,36 +2,22 @@
 
 namespace JustBetter\MagentoPrices\Actions\Utility;
 
-use Illuminate\Http\Client\Response;
-use Illuminate\Support\Enumerable;
 use JustBetter\MagentoClient\Client\Magento;
 use JustBetter\MagentoPrices\Contracts\Utility\DeletesCurrentSpecialPrices;
-use JustBetter\MagentoPrices\Models\Price;
 
 class DeleteCurrentSpecialPrices implements DeletesCurrentSpecialPrices
 {
     public function __construct(protected Magento $magento) {}
 
-    public function delete(Price $price): void
+    public function delete(array $skus): void
     {
         $this->magento
-            ->post('products/special-price-information', ['skus' => [$price->sku]])
+            ->post('products/special-price-information', ['skus' => $skus])
             ->throw()
             ->collect()
-            ->chunk(20)
-            ->each(function (Enumerable $specialPrices) use ($price): void {
+            ->each(function (array $specialPrices): void {
                 $this->magento
-                    ->post('products/special-price-delete', ['prices' => $specialPrices->toArray()])
-                    ->onError(function (Response $response) use ($price, $specialPrices): void {
-                        activity()
-                            ->on($price)
-                            ->useLog('error')
-                            ->withProperties([
-                                'response' => $response->body(),
-                                'payload' => $specialPrices->toArray(),
-                            ])
-                            ->log('Failed to remove special price');
-                    })
+                    ->post('products/special-price-delete', ['prices' => $specialPrices])
                     ->throw();
             });
     }
